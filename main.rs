1cdf048287f37cd86716f17f4c8bb1bc48fad5fe
beforecode <yassineuniversite@gmail.com>
2022-10-03 21:34:18 +0100

    working on type_of()

--- main.rs
+++ main.rs
@@ -1,7 +1,8 @@
-// use std::net::TcpListener;
+// use std::net::{TcpListener};
 
 fn main() {
-	let _server = TcpListener::bind("127.0.0.1:8080")
-	.expect("Task Failed");
-	println!("Server Is Running On Port 8080");
+	// let _server = TcpListener::bind("127.0.0.1:8080");
+	// println!("TCP Server Is OK ON Port 8080");
+	let x:i8 = 5;
+	println!("{:?}", type_of(x));
 }
