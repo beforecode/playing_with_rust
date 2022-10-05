7a651c4b4bc62653ba5fcd2c4cc7f22754ab78a8
beforecode <yassineuniversite@gmail.com>
2022-10-05 13:31:25 +0100

    Rust server is now sending response back to brower, not to Postman

--- main.rs
+++ main.rs
@@ -1,46 +1,42 @@
 use std::net::{TcpListener, TcpStream};
+use std::io::Write;
 use std::io::Read;
-// use std::fs;
+use std::fs;
 
 fn main() {
 	let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-	println!("Server is Ok");
-	let x = "GET / HTTP/1.1\r\n".as_bytes();
-	println!("{:?}", x);
+	println!("Server is OK on port 7878");
 	for stream in listener.incoming() {
 		let stream = stream.unwrap();
 		handel_connection(stream);
 	}
+	
 }
 
 fn handel_connection(mut stream: TcpStream) {
-	// let mut buffer : Vec<u8> = vec![0; 1024];
-	// stream.read(&mut buffer).unwrap();
-	// let get_request = b"GET / HTTP/1.1\r\n";
-
 	let mut buffer:Vec<u8> = vec![0; 1024];
 	stream.read(&mut buffer).expect("Reading Buffer Failed");
-	// println!("{:?}", &buffer);
+	
+	let get_request = b"GET / HTTP/1.1\r\n";
 
-
-
-
-
-   let get_request = b"GET / HTTP/1.1\r\n";
-
-
-
-
-
-
-   
-   
    if buffer.starts_with(get_request) {
-   	println!("1");
+   	
+   	let file = fs::read_to_string("./pages/index.html").expect("Faild to read Inedx File");
+   	let status_line = "HTTP/1.1 200 OK";
+   	
+   	let response = format!(
+   		"{}\r\nContent-Length{}\r\n\r\n{}",
+   		status_line,   		
+   		file.len(),
+   		file);
+
+
+   	stream.write(response.as_bytes()).expect("Failed to write to stream");
+   	stream.flush().expect("Failed to flush");
+
    } else {
    	println!("Unaccepted HTTP Request");
    }
-
 
 	// let contents = fs::read_to_string(filename).unwrap();
 	// let response = format!(
