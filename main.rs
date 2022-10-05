1bb5b3954ccc47dc2faf29eac49f8844653cc862
beforecode <yassineuniversite@gmail.com>
2022-10-05 23:26:01 +0100

    successfully using custom traits

--- main.rs
+++ main.rs
@@ -1,50 +1,62 @@
-use std::io::BufRead;
-use std::io::BufReader;
-use std::net::{TcpListener, TcpStream};
-use std::io::Write;
-use std::io::Read;
-use std::fs;
+// use std::io::BufRead;
+// use std::io::BufReader;
+// use std::net::{TcpListener, TcpStream};
+// use std::io::Write;
+// use std::io::Read;
+// use std::fs;
+// ________________
 
 fn main() {
-	let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-	println!("Server is OK on port 7878");
-	for stream in listener.incoming() {
-		let stream = stream.unwrap();
-		handel_connection(stream);
+	// let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+	// println!("Server is OK on port 7878");
+	// for stream in listener.incoming() {
+	// 	let stream = stream.unwrap();
+	// 	handel_connection(stream);
+	// }	
+
+	pub struct Movie {
+		pub title: String,
+		pub release: u32,
+		pub director: String
 	}
-	
-	let mut file = fs::File::open("./pages/index.html").unwrap();
-	let bufferd_file = BufReader::new(&mut file);
-	eprintln!("{:#?}", bufferd_file.lines());
-	
-	
-}
 
-fn handel_connection(mut stream: TcpStream) {
-	let mut buffer:Vec<u8> = vec![0; 1024];
-	stream.read(&mut buffer).expect("Reading Buffer Failed");
+	trait Details {
+		fn age(&self) -> u32;
+		fn directors_name(&self) -> &str;
+	}
 
+	impl Details for Movie {
+		fn age(&self) -> u32 {
+			let age:u32 = 2022 - &self.release;
+			println!("This movie was released {} years age", age);
+			age
+		}
+		fn directors_name(&self) -> &str {
+			let dname = &self.director;
+			println!("The name of the director is {}", dname);
+			dname
+		}
+	}
 
-	let get_request = b"GET / HTTP/1.1\r\n";
+	let titanic_movie = Movie {
+		title: String::from("Titanic"),
+		release: 1992,
+		director: String::from("Yassine")
+	};
 
-   if buffer.starts_with(get_request) {
-   	
-   	let file = fs::read_to_string("./pages/index.html").expect("Faild to read Inedx File");
-   	let status_line = "HTTP/1.1 200 OK";
-   	
-   	let response = format!(
-   		"{}\r\nContent-Length{}\r\n\r\n{}",
-   		status_line,   		
-   		file.len(),
-   		file);
-
-
-   	stream.write(response.as_bytes()).expect("Failed to write to stream");
-   	stream.flush().expect("Failed to flush");
-
-   } else {
-   	println!("Unaccepted HTTP Request");
-   }
+	Details::age(&titanic_movie);
+	Details::directors_name(&titanic_movie);
 
 }
+
+// fn handel_connection(mut stream: TcpStream) {
+	// let stream_buf = BufReader::new(&mut stream);
+	// let res = stream_buf
+	// .lines()
+	// .map(|resault| resault.expect("Task Failed Due To Underlying OS") )
+	// .take_while(|item| !item.is_empty())
+	// .collect::<Vec<_>>();
+
+	// println!("{:#?}", &stream_buf.lines());
+// }
 
