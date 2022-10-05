70b6ef76f332f2f2924f0a20d62b09af03cd290d
beforecode <yassineuniversite@gmail.com>
2022-10-05 18:50:04 +0100

    staging commit everthing is OK

--- main.rs
+++ main.rs
@@ -1,50 +1,50 @@
-// use std::io::BufRead;
+use std::io::BufRead;
 use std::io::BufReader;
-// use std::net::{TcpListener, TcpStream};
-// use std::io::Write;
-// use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+use std::io::Write;
+use std::io::Read;
 use std::fs;
 
 fn main() {
-	// let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-	// println!("Server is OK on port 7878");
-	// for stream in listener.incoming() {
-	// 	let stream = stream.unwrap();
-	// 	handel_connection(stream);
-	// }
+	let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+	println!("Server is OK on port 7878");
+	for stream in listener.incoming() {
+		let stream = stream.unwrap();
+		handel_connection(stream);
+	}
+	
 	let mut file = fs::File::open("./pages/index.html").unwrap();
 	let bufferd_file = BufReader::new(&mut file);
-	eprintln!("{:#?}", &bufferd_file);
+	eprintln!("{:#?}", bufferd_file.lines());
+	
 	
 }
 
-// fn handel_connection(mut stream: TcpStream) {
-// 	let mut buffer:Vec<u8> = vec![0; 1024];
-// 	stream.read(&mut buffer).expect("Reading Buffer Failed");
+fn handel_connection(mut stream: TcpStream) {
+	let mut buffer:Vec<u8> = vec![0; 1024];
+	stream.read(&mut buffer).expect("Reading Buffer Failed");
 
 
-// 	println!("{:#?}", stream);
-	
-// 	let get_request = b"GET / HTTP/1.1\r\n";
+	let get_request = b"GET / HTTP/1.1\r\n";
 
-//    if buffer.starts_with(get_request) {
+   if buffer.starts_with(get_request) {
    	
-//    	let file = fs::read_to_string("./pages/index.html").expect("Faild to read Inedx File");
-//    	let status_line = "HTTP/1.1 200 OK";
+   	let file = fs::read_to_string("./pages/index.html").expect("Faild to read Inedx File");
+   	let status_line = "HTTP/1.1 200 OK";
    	
-//    	let response = format!(
-//    		"{}\r\nContent-Length{}\r\n\r\n{}",
-//    		status_line,   		
-//    		file.len(),
-//    		file);
+   	let response = format!(
+   		"{}\r\nContent-Length{}\r\n\r\n{}",
+   		status_line,   		
+   		file.len(),
+   		file);
 
 
-//    	stream.write(response.as_bytes()).expect("Failed to write to stream");
-//    	stream.flush().expect("Failed to flush");
+   	stream.write(response.as_bytes()).expect("Failed to write to stream");
+   	stream.flush().expect("Failed to flush");
 
-//    } else {
-//    	println!("Unaccepted HTTP Request");
-//    }
+   } else {
+   	println!("Unaccepted HTTP Request");
+   }
 
-// }
+}
 
